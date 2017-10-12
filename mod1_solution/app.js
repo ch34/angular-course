a37a284c828ed3b0460e6f72db9bc95e62eaf0dc
Muhammad Chaudhry <mchaud16@jhu.edu>
2017-10-11 23:08:17 -0400

    Final Commit!

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -10,26 +10,34 @@ function LunchCheckController($scope) {
 
   $scope.checkInput = function () {
     if($scope.menu == '') {
-      printMessage('Please enter data first');
+      printMessage('Please enter data first', 'red');
     } else {
       var input = $scope.menu;
       var inputArray = input.split(',');
       var numberOfItems = numItems(inputArray);
 
       if(numberOfItems == 0) {
-        printMessage('Please enter data first');
+        printMessage('Please enter data first', 'red');
       }else {
         if(numberOfItems > 3) {
-          printMessage('Too much!');
+          printMessage('Too much!', 'green');
         } else {
-          printMessage('Enjoy!');
+          printMessage('Enjoy!', 'green');
         }
       }
     }
   };
 
-  function printMessage(message) {
+  function printMessage(message, color) {
     $scope.message = message;
+    $scope.fontStyle = {
+      'color': color
+    };
+    $scope.inputBoxStyle = {
+      'border-color': color,
+      'border-style': 'solid'
+
+    };
   }
 
   function numItems(itemArray) {
