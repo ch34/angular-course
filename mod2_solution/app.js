512f72481dfac3c00b8b327741b050a356399fdb
Muhammad Chaudhry <mchaud16@jhu.edu>
2017-10-18 22:50:32 -0400

    Most of it done, just testing

--- mod2_solution/app.js
+++ mod2_solution/app.js
@@ -2,40 +2,58 @@
 'use strict';
 
 angular.module('ShoppingListCheckOff', [])
-.controller('CounterController', CounterController);
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-CounterController.$inject = ['$scope'];
-function CounterController($scope) {
-  $scope.onceCounter = 0;
-  $scope.counter = 0;
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var buyCtrl = this;
 
-  $scope.showNumberOfWatchers = function () {
-    console.log('Number of watchers: ' + $scope.$$watchersCount);
+  buyCtrl.items = ShoppingListCheckOffService.getToBuyItems();
 
+  buyCtrl.boughtItem = function (itemIndex) {
+    ShoppingListCheckOffService.boughtItem(itemIndex);
+  };
+}
+
+
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+  var boughtCtrl = this;
+
+  boughtCtrl.items = ShoppingListCheckOffService.getBoughtItems();
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of items to buy
+  var toBuyItems = [
+    { name: "cookies", quantity: 2 },
+    { name: "chips", quantity: 4 },
+    { name: "candies", quantity: 10 },
+    { name: "soda", quantity: 1 },
+    { name: "apples", quantity: 5 }
+  ];
+
+  // List of bought items
+  var boughtItems = [];
+
+  service.boughtItem = function (itemIndex) {
+    var item = toBuyItems[itemIndex];
+    toBuyItems.splice(itemIndex, 1);
+    boughtItems.push(item);
   };
 
-  $scope.countOnce = function() {
-    $scope.onceCounter = 1;
+  service.getToBuyItems = function () {
+    return toBuyItems;
   };
 
-  $scope.upCounter = function() {
-    $scope.counter++;
-  }
-
-  $scope.$watch(function () {
-    console.log("Digest Loop Fired!!");
-
-  })
-  // $scope.$watch('onceCounter', function (newValue, oldValue) {
-  //   console.log("onceCounter old value: ", oldValue);
-  //    console.log("onceCounter new value: ", newValue);
-  // });
-  //
-  // $scope.$watch('counter', function (newValue, oldValue) {
-  //   console.log("counter old value: ", oldValue);
-  //    console.log("counter new value: ", newValue);
-  // });
-
+  service.getBoughtItems = function () {
+    return boughtItems;
+  };
 }
 
 })();
