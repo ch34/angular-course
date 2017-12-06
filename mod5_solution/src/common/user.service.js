e58aeb51c152e74474c36539201f955fd1587e26
Muhammad Chaudhry <ch34@umbc.edu>
2017-12-05 22:39:16 -0500

    Validation works on loose focus and stored info in service

---
+++ mod5_solution/src/common/user.service.js
@@ -0,0 +1,22 @@
+(function () {
+"use strict";
+
+angular.module('common')
+.service('UserService', UserService);
+
+
+// UserService.$inject = ['$http', 'ApiPath'];
+function UserService() {
+  var service = this;
+
+  var user;
+
+  service.setUser = function (userInfo) {
+    user = userInfo;
+  };
+
+}
+
+
+
+})();
--- mod5_solution/src/public/sign-up/sign-up.controller.js
+++ mod5_solution/src/public/sign-up/sign-up.controller.js
@@ -4,25 +4,38 @@
 angular.module('public')
 .controller('SignUpController', SignUpController);
 
-SignUpController.$inject = ['MenuService'];
-function SignUpController(MenuService) {
+SignUpController.$inject = ['MenuService', 'UserService'];
+function SignUpController(MenuService, UserService) {
   // var $ctrl = this;
   // $ctrl.menuCategories = menuCategories;
 
   var reg = this;
 
+  reg.validFavDish = false;
+  reg.errorMessage = "";
+
   reg.submit = function () {
     console.log("IN SUBMIT: ");
     reg.completed = true;
+
+  };
+
+  reg.checkFavDish = function () {
     var promise = MenuService.getFavoriteDish(reg.user.favoritedish);
     promise.then(function (response) {
       console.log("RESPONSE" + response.data);
+      reg.validFavDish = true;
     })
     .catch(function (error) {
       reg.errorMessage = "No such menu number exists";
       console.log("Something went terribly wrong.");
+      reg.validFavDish = false;
     });
-  };
+  }
+
+  reg.setUser = function () {
+    UserService.setUser(reg.user);
+  }
 }
 
 
